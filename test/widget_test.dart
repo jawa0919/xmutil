4952cce727e76c9a9f0beab0ae452fa014e98c17
jawa0919 <jawa0919@163.com>
2025-12-12 09:20:52 +0800

    test: 将测试文件指向新的测试入口文件
    将 widget_test.dart 中的导入从 main.dart 改为 test_main.dart
    添加新的测试入口文件 test_main.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:xmutil/main.dart';
+import 'package:xmutil/test_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
